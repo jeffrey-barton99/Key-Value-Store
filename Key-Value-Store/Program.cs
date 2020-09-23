ce54dd4abe267a3b81a3ad20e23c56235d801b03
Jeffrey Barton <jeffrey.barton99@outlook.com>
2020-09-22 22:20:33 -0500

    initial commit

---
+++ Key-Value-Store/Program.cs
@@ -0,0 +1,86 @@
+﻿using Key_Value_Store;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Key_Value_Store
+{
+    struct KeyValue
+    {
+        public readonly string Key;
+        public readonly object Value;
+
+        public KeyValue(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }   
+    }
+
+    class MyDictionary
+    {
+        int storedValues = 0;
+
+        KeyValue[] kvs = new KeyValue[5];
+        
+       public object this[string key]
+        {
+            get => GetValueAtKey(key);
+            set => AddOrReplaceKeyValue(new KeyValue(key, value));
+        }
+        
+        public object GetValueAtKey(string key)
+        {
+            for (int i = 0; i < storedValues; i++)
+            {
+                if (key == kvs[i].Key)
+                {
+                    return kvs[i].Value;
+                }
+            }
+
+            throw new KeyNotFoundException();
+        }
+
+        public void AddOrReplaceKeyValue(KeyValue newKeyValue)
+        {
+            bool foundMatchingKey = false;
+
+            for (int i = 0; i < storedValues; i++)
+            {
+                if (newKeyValue.Key == kvs[i].Key)
+                {
+                    foundMatchingKey = true;
+                    kvs[i] = newKeyValue;
+                }
+            }
+
+            if (!foundMatchingKey)
+            {
+                kvs[storedValues++] = newKeyValue;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+        
+    
+}
